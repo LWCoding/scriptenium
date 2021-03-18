59bf2d4a3c168a1fb2061ad58ea809998cbea5ce
Lucas Wang <lwangprivate@gmail.com>
2021-03-17 20:57:37 -0500

    Capture all routes

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.static(path.join(__dirname, 'dist')));
 const userRouter = require("./routers/users.js")
 app.use(userRouter)
 
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+  })
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}.`)
 });
